07b4456655741dae22985af7c114aeb484f60ce8
Mohammed Haseeb <haseebsafdar870@email.com>
2026-09-10 03:52:18 +0530

    "The project of 'Bank Account is complited'"

--- B/b.py
+++ B/b.py
@@ -1,6 +1,42 @@
+class balanceexception(Exception) :
+    pass
 class bankaccount :
     def __init__(self,accname,initialammount):
         self.name = accname
         self.balance = initialammount
-        print(f"\nAccount {self.name} is created with balance = ${self.balance}")
+        print(f"\nAccount {self.name} is created \nBalance = &{self.balance:.2f}")
+
+
+    def getbalance(self):
+        print(f"\nAccount '{self.name}' Balance = '&{self.balance:.2f}'")
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        print(f"\nDeposit complete.")
+        self.getbalance()
+
+    def viabletransaction(self,amount):
+        if self.balance >= amount:
+            return
+        else :
+            raise balanceexception(
+                f"\nSorry, account'{self.name}' has only balance '{self.balance:.2f}"
+            )
+    def withdraw(self,amount):
+        try :
+            self.viabletransaction(amount)
+            self.balance= self.balance - amount
+            print("\n withdraw complete")
+            self.getbalance()
+        except balanceexception as error:
+            print(f"\nWithdraw interrupted : '{error}'")
+
+    def transfer(self,amount,account):
+        try :
+            self.viabletransaction(amount)
+            self.withdraw(amount)
+            account.deposit(amount)
+        except balanceexception as error :
+            print(f"\nTransfer intrerupted , {error}")
+
 
--- B/mainfile.py
+++ B/mainfile.py
@@ -1,4 +1,14 @@
-from b import bankaccount 
+from b import bankaccount
 
 haseeb = bankaccount("Mohammed_haseeb",100010)
 haseeb2 = bankaccount("Mohammed_haseeb",150000)
+
+haseeb.getbalance()
+haseeb2.getbalance()
+
+haseeb.deposit(100010)
+haseeb2.deposit(15000)
+
+haseeb.withdraw(100)
+
+haseeb.transfer(100,haseeb2)
